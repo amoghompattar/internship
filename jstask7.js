b10cecb7a03b290d87f247b04e754720ccf3b241
amoghompattar <167611330+amoghompattar@users.noreply.github.com>
2024-05-24 15:11:25 +0530

    Add files via upload

---
+++ jstask7.js
@@ -0,0 +1,14 @@
+const JsUser = {
+    name: "amogh",
+    fullName: "amogh pattar",
+    age: 21,
+    gender:"Male",
+    location: "hubli",
+    email: "amoghpattar@gmail.com",
+    islogedIn: false,
+    lastLoginDay: ["Monday", "Saturday"],
+    func: function () {
+      return this.firstName + " " + this.lastName;
+    },
+  };
+  
